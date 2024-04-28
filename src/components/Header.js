924d8090af052e17a8c58e7bff5e0e16bb1bee2f
Seba <sebaq3@gmail.com>
2024-04-28 23:41:50 +0200

    asd

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+// Header.js
+import React from 'react';
+
+function Header() {
+    return (
+        <header>
+            <h1>Mi Portafolio</h1>
+            <nav>
+                <ul>
+                    <li><a href="#about">Sobre Mí</a></li>
+                    <li><a href="#projects">Proyectos</a></li>
+                    <li><a href="#contact">Contacto</a></li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
