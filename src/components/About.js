386f04d494c858b9fd772832ad5466bc355629c3
Seba <sebaq3@gmail.com>
2024-05-02 21:19:09 +0200

    prueba agregar texto

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ function About({ language }) {
     const content = {
         es: {
             title: "Sobre Mí",
-            description: "Mi nombre es [Tu Nombre]. Soy un desarrollador web apasionado por la creación de experiencias digitales increíbles. ..."
+            description: "Mi nombre es Sebastian. Soy un desarrollador web apasionado por la creación de experiencias digitales increíbles. Estoy creando una Web a modo de ejemplo usando ReactJS y un servidor de mail con python"
         },
         en: {
             title: "About Me",
