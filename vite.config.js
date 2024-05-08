93725ff0c4b5ec8fb3887a16e3054ee82ca49ed5
Seba <sebaq3@gmail.com>
2024-05-08 23:13:29 +0200

    analytics

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     VitePluginRadar({
       // Google Analytics tag injection
       analytics: {
-        id: process.env.GOOGLE_ANALYTICS_ID || 'G-XXXXA',
+        id: process.env.GOOGLE_ANALYTICS_ID || 'G-S6ZFR5W1VN',
       },
     })
   ],
