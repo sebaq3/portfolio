753137f9ba7935c081b1101ecddffcc757a88995
Seba <sebaq3@gmail.com>
2024-05-08 23:03:47 +0200

    env

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -4,15 +4,21 @@ import '../styles/inicio.css';
 import { useStateContext } from '../contexts/StateContext';
 
 const Inicio = ({ show }) => {
-
+	const asd = import.meta.env.VITE_PRUEBA_SECRETA;
+	const asd2 = import.meta.env.PRUEBA_SECRETA;
 	const { showInicio } = useStateContext();
+	console.log(asd);
+	console.log(asd2);
     return (    
-    	<div className={`inicio ${show ? 'fade-in' : ''}`}>
-        	<video autoPlay muted loop className="video-background">
-          		<source src="https://qanplatform.com/_nuxt/qan-anim-02.bc047b0d.mp4" type="video/mp4" />
-          		Tu navegador no soporta videos HTML5.
-        	</video>
-    </div>
+    	<>
+			
+			<div className={`inicio ${show ? 'fade-in' : ''}`}>
+				<video autoPlay muted loop className="video-background">
+					<source src="https://qanplatform.com/_nuxt/qan-anim-02.bc047b0d.mp4" type="video/mp4" />
+					Tu navegador no soporta videos HTML5.
+				</video>	
+			</div>
+		</>
   );
 }
 
