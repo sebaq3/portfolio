a1a757e151691b1c218ec0a1830b14b5021cf066
Seba <sebaq3@gmail.com>
2024-05-17 13:07:33 +0200

    styles

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLanguage } from '../contexts/LanguageContext';
 import '../styles/projects.css';
 
 const Projects = ({ show }) => {
   const { language } = useLanguage();
+  const [selectedProject, setSelectedProject] = useState(null);
+
+
   if (!show) {
     return null;
   }
@@ -13,21 +16,35 @@ const Projects = ({ show }) => {
       id: 1,
       title: language === 'es' ? 'Este proyecto React-Vite' : 'This project React-Vite',
       imageUrl: 'https://raw.githubusercontent.com/leungwensen/svg-icon/8b84d725b0d2be8f5d87cac7f2c386682ce43563/dist/svg/logos/react.svg',
-      link: 'https://github.com/sebaq3/portfolio'
+      link: 'https://github.com/sebaq3/portfolio',
+      description: language === 'es' ? 'Descripción en español para el proyecto React-Vite.' : 'Description in English for the React-Vite project.',
+      descriptionImage: "/linkedin.png"
     },
     {
       id: 2,
       title: language === 'es' ? 'Python ALPR' : 'Python ALPR',
       imageUrl: 'https://icon.icepanel.io/Technology/svg/TensorFlow.svg',
-      link: 'https://github.com/sebaq3/anlpr'
+      link: 'https://github.com/sebaq3/anlpr',
+      description: language === 'es' ? 'Descripción en español para el proyecto Python ALPR.' : 'Description in English for the Python ALPR project.',
+      descriptionImage: "/linkedin.png"
     },
     {
       id: 3,
       title: language === 'es' ? 'Proyecto Personal C++' : 'Personal Project C++',
       imageUrl: 'https://icon.icepanel.io/Technology/svg/C%2B%2B-%28CPlusPlus%29.svg',
-      link: 'https://github.com/sebaq3/cortina'
+      link: 'https://github.com/sebaq3/cortina',
+      description: language === 'es' ? 'Descripción en español para el proyecto Personal C++.' : 'Description in English for the Personal Project C++.',
+      descriptionImage: "/linkedin.png"
     }
   ];
+
+  const handleMouseEnter = (project) => {
+    setSelectedProject(project);
+  };
+
+  const handleMouseLeave = () => {
+    setSelectedProject(null);
+  };
 
   return (
     <div className="projects">
@@ -35,11 +52,22 @@ const Projects = ({ show }) => {
       <div className="content">
         <div className="project-container">
           {projectsData.map(project => (
-            <div key={project.id} className="project">
+            <div 
+              key={project.id} 
+              className="project"
+              onMouseEnter={() => handleMouseEnter(project)}
+              onMouseLeave={handleMouseLeave}
+            >
               <a href={project.link} target="_blank" rel="noopener noreferrer">
                 <img className="project-image" src={project.imageUrl} alt={project.title} />
               </a>
               <p>{project.title}</p>
+              {selectedProject && selectedProject.id === project.id && (
+                <div className="project-details">
+                  <p>{selectedProject.description}</p>
+                  <img className='project-detail-image' src={selectedProject.descriptionImage} alt="alt" />
+                </div>
+              )}
             </div>
           ))}
         </div>
